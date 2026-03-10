6fb4ff7d8b85d4dc8f98569316111a2c22894939
Open Open <a0937222859@gmail.com>
2026-03-10 10:46:02 +0800

    fix: add package metadata to root Cargo.toml
    - Add [package] section to make cargo run work
    - Add [[bin]] target for echovoice binary
    - Verify cargo run displays model list correctly
    
    Refs: core module fix

---
+++ target/debug/deps/echovoice-3f639df6cdccf195.d
@@ -0,0 +1,5 @@
+/Users/openopen/Code/projects/echovoice/target/debug/deps/echovoice-3f639df6cdccf195.d: src/main.rs
+
+/Users/openopen/Code/projects/echovoice/target/debug/deps/echovoice-3f639df6cdccf195: src/main.rs
+
+src/main.rs:
---
+++ target/debug/echovoice.d
@@ -0,0 +1,1 @@
+/Users/openopen/Code/projects/echovoice/target/debug/echovoice: /Users/openopen/Code/projects/echovoice/src/main.rs
